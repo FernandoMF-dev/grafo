e73fa77cddb04e37f7de4906097a0e3963237735
Fernando Martins Ferreira <fernando.ferreira@basis.com.br>
2021-11-24 14:08:13 -0300

    Criando projeto

---
+++ grafo/cmake-build-debug/CMakeFiles/grafo.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/grafo.dir/main.c.o"
+  "grafo"
+  "grafo.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/grafo.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
